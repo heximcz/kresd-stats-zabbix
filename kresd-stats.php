e1bd7bb5b22a4be70fa4df6e75b1a220cc6ec869
hexim <hexim@hexim.cz>
2018-04-14 13:12:47 +0200

    reformat code

--- kresd-stats.php
+++ kresd-stats.php
@@ -6,9 +6,9 @@ use KresdStatsZabbix\Application\Config\LoadConfig;
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'vendor/autoload.php';
 
 try {
-    $app = new StatsApplication( new LoadConfig(),'Knot statistics for Zabbix', '0.1');
+    $app = new StatsApplication( new LoadConfig(), 'Knot statistics for Zabbix', '0.1');
     $app->loadCommands();
     $app->run();
-} catch (Exception $exception) {
+} catch (\Exception $exception) {
     echo 'Caught exception: ' . $exception->getMessage() . PHP_EOL;
 }
